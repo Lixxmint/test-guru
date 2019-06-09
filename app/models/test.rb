cd7fb0de08c92ac48c49ea22bdd56bb4db02e21b
lixxmint <the.lixxmint@metro-cc.ru>
2019-06-09 21:09:58 +0300

    create metod in test, user model

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,9 @@
 class Test < ApplicationRecord
-  def self.titles_by_category(category)
-    Test.by_category(category).order(title: :desc).pluck(:title)
+
+def self.ordered_test_titles_by_category(category)
+  Test.joins('JOIN categories ON categories.id = tests.category_id')
+    .where('categories.title = :category', category: category)
+    .order('tests.title DESC')
+    .pluck('tests.title')
   end
 end
